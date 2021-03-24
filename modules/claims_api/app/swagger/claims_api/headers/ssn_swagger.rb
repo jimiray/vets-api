cd0e2dfe9c76218ce2d64f06fed1fc492b1e5860
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-03-24 18:04:49 -0500

    Introduce the "ClaimsApi::Headers::SsnSwagger" component

---
+++ modules/claims_api/app/swagger/claims_api/headers/ssn_swagger.rb
@@ -0,0 +1,17 @@
+module ClaimsApi
+  module Headers
+    class SsnSwagger
+      include Swagger::Blocks
+
+      swagger_component do
+        parameter :ssn_header do
+          key :name, 'X-VA-SSN'
+          key :in, :header
+          key :description, 'SSN of Veteran being represented'
+          key :required, true
+          key :type, :string
+        end
+      end
+    end
+  end
+end
